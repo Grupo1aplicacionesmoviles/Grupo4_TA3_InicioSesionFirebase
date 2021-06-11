65b27ef8c721b1d10a9911407d27dac6800f49bb
lesterdc <50631932+lesterdc@users.noreply.github.com>
2021-06-11 09:09:46 -0700

    Add files via upload
    Primer commit

---
+++ Grupo4_TA3_InicioSesionFirebase/app/src/main/java/com/example/grupo4_ta3_iniciosesionfirebase/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.grupo4_ta3_iniciosesionfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+    }
+}
